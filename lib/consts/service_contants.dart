d543c40450a6dd929bc5a9759ae951cdc4dc31ab
senpai <raohuraira331.rb@gmail.com>
2024-01-26 21:05:03 +0500

    bugs fixed

--- lib/consts/service_contants.dart
+++ lib/consts/service_contants.dart
@@ -4,14 +4,10 @@ const signIn = "/api/signin";
 const updUser = "/api/updateUser";
 
 // explore routes
-const addCategory = "/api/addCategory";
 const getCategory = "/api/getCategory";
 const allCategories = "/api/getAllCategories";
 
 // item routes
-const addItem = "/api/addItem";
 const getItem = "/api/getItem";
 const allItems = "/api/getAllItems";
-const deleteItem = "/api/deleteItem";
-const updateItem = "/api/updateItem";
 const getItemDetails = "/api/getItemDetails";
